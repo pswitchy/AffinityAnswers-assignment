33de20fc98784146f002e341339368f1f78fc3dc
Parth Sharma <99757071+pswitchy@users.noreply.github.com>
2025-05-14 19:04:25 +0530

    Create question_1.sql

---
+++ question_1.sql
@@ -0,0 +1,11 @@
+SELECT species, ncbi_id
+FROM taxonomy
+WHERE species LIKE 'Panthera tigris%';
+
+SELECT COUNT(DISTINCT species) AS number_of_tiger_types
+FROM taxonomy
+WHERE species LIKE 'Panthera tigris%';
+
+SELECT ncbi_id
+FROM taxonomy
+WHERE species = 'Panthera tigris sumatrae';
